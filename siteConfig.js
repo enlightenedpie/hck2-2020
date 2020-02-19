05a3ad585c86fcf84a96c2d6ebb7e456ae542b54
David Paul Crouch <david.crouch@hck2.com>
2020-02-19 11:26:57 -0600

    initial commit from me

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  name: 'London After Midnight',
-  shortName: 'LAM',
+  name: 'HCK2 Partners',
+  shortName: 'HCK2',
   description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
+  url: 'https://hck2.dev', // Domain of your site without prefix!
   prefix: '/',
   author: 'Vaporwavy',
   twitter: ''
