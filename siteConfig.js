e2dfbc0c6de29ad4a25fe04b90199dd6b1d678d7
David Paul Crouch <david.crouch@hck2.com>
2020-02-20 15:07:34 -0600

    feat(pages): updated site config

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'HCK2 Partners',
   shortName: 'HCK2',
-  description: 'HCK2 Partners\' own marketing website, replete with blogs, case studies, and creative briefs on all of our work.',
+  description: 'HCK2 Partners\' own marketing woobsite, replete with blogs, case studies, and creative briefs on all of our work.',
   url: 'https://hck2.dev', // Domain of your site without prefix!
   prefix: '/',
   author: 'David Paul Crouch',
