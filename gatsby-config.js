d3d834a452dbe74241c4c3cedc5d47f217453bcb
David Paul Crouch <david.crouch@hck2.com>
2020-02-24 19:37:40 -0600

    fix: 'd an oopsie

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,27 @@ module.exports = {
     },
   },
   plugins: [
-    /* {
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
         name: `blog`,
       },
-    }, */
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages`,
+        name: `pages`,
+      },
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
