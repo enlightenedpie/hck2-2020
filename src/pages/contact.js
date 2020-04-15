0dbebaed3aeefc13dbe9c073be664f0e4e697c71
David Carr <david.carr@hck2.com>
2020-04-15 17:21:33 -0500

    Hook Contact form up to Netlify.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../templates/layout"
 import styles from "./contact.module.sass"
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
 const ContactPage = props => {
+  let [subd, updSubd] = useState(false)
+  let [state, setState] = useState({})
+
+  setState = e => Object.assign(state, { [e.target.name]: e.target.value })
+
   return (
     <Layout {...props} title={"contact page"}>
       <div className={[styles.container_fluid, styles.bg_blue].join(" ")}>
@@ -34,63 +45,88 @@ const ContactPage = props => {
                 styles.border_right,
               ].join(" ")}
             >
-              <form>
-                <div className={styles.form_group}>
-                  <label>
-                    First Name<sup>*</sup>
-                  </label>
-                  <input type="text" name="first_name" />
-                </div>
-
-                <div className={styles.form_group}>
-                  <label>
-                    Last Name<sup>*</sup>
-                  </label>
-                  <input type="text" name="last_name" />
-                </div>
-
-                <div className={styles.form_group}>
-                  <label>
-                    Inquiry Type<sup>*</sup>
-                  </label>
-                  <div className={styles.select_wrapper}>
-                    <select name="type">
-                      <option value="general">General Information</option>
-                      <option value="rfp">Request for Proposal</option>
-                      <option value="career">Career Opportunities</option>
-                    </select>
+              {subd ? (
+                <span>Thx bro!</span>
+              ) : (
+                <form
+                  name="hck2contact"
+                  className={styles.newsletterForm}
+                  onSubmit={e => {
+                    fetch("/", {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                      },
+                      body: encode({ "form-name": "hck2contact", ...state }),
+                    })
+                      .then(() => {
+                        console.log("submitted")
+                        updSubd(true)
+                      })
+                      .catch(err => alert(err))
+                    e.preventDefault()
+                  }}
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                >
+                  <div className={styles.form_group}>
+                    <label htmlFor="first_name">
+                      First Name<sup>*</sup>
+                    </label>
+                    <input type="text" name="first_name" />
                   </div>
-                </div>
 
-                <div className={styles.form_group}>
-                  <label>
-                    Organization<sup>*</sup>
-                  </label>
-                  <input type="text" name="organization" />
-                </div>
+                  <div className={styles.form_group}>
+                    <label htmlFor="last_name">
+                      Last Name<sup>*</sup>
+                    </label>
+                    <input type="text" name="last_name" />
+                  </div>
 
-                <div className={styles.form_group}>
-                  <label>
-                    Email<sup>*</sup>
-                  </label>
-                  <input type="email" name="email" />
-                </div>
+                  <div className={styles.form_group}>
+                    <label htmlFor="type">
+                      Inquiry Type<sup>*</sup>
+                    </label>
+                    <div className={styles.select_wrapper}>
+                      <select name="type">
+                        <option value="general">General Information</option>
+                        <option value="rfp">Request for Proposal</option>
+                        <option value="career">Career Opportunities</option>
+                      </select>
+                    </div>
+                  </div>
 
-                <div className={styles.form_group}>
-                  <label>Phone</label>
-                  <input type="phone" name="phone" />
-                </div>
+                  <div className={styles.form_group}>
+                    <label htmlFor="organization">
+                      Organization<sup>*</sup>
+                    </label>
+                    <input type="text" name="organization" />
+                  </div>
 
-                <div className={styles.form_group}>
-                  <label>Question / Comments</label>
-                  <textarea name="comments"></textarea>
-                  <p className={styles.small}>*Required</p>
-                </div>
+                  <div className={styles.form_group}>
+                    <label htmlFor="email">
+                      Email<sup>*</sup>
+                    </label>
+                    <input type="email" name="email" />
+                  </div>
 
-                <div className={styles.form_group}>
-                  <input type="submit" value="SUBMIT" />
-                </div>
-              </form>
+                  <div className={styles.form_group}>
+                    <label htmlFor="phone">Phone</label>
+                    <input type="phone" name="phone" />
+                  </div>
+
+                  <div className={styles.form_group}>
+                    <label htmlFor="comments">Question / Comments</label>
+                    <textarea name="comments"></textarea>
+                    <p className={styles.small}>*Required</p>
+                  </div>
+
+                  <input type="hidden" name="form-name" value="hck2contact" />
+                  <div className={styles.form_group}>
+                    <input type="submit" value="SUBMIT" />
+                  </div>
+                </form>
+              )}
             </div>
             <div className="col-12 col-md-6">
               <h2>LOCATION</h2>
@@ -106,7 +142,7 @@ const ContactPage = props => {
   )
 }
 
-export default ContactPage
+// export default ContactPage
 
 const mapQuery = graphql`
   query {
