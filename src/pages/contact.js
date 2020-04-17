4bed61b92f2eff9c383196520cada4e95a223647
David Carr <david.carr@hck2.com>
2020-04-16 20:31:47 -0500

    fix responsive layout.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -46,91 +46,93 @@ const ContactPage = ({ props, staticMap }) => {
                 styles.col_12,
                 styles.col_md_6,
                 styles.col,
-                styles.border_right,
+                styles.column_border,
               ].join(" ")}
             >
-              {subd ? (
-                <span>Thx bro!</span>
-              ) : (
-                <form
-                  name="hck2contact"
-                  className={styles.newsletterForm}
-                  onSubmit={e => {
-                    fetch("/", {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                      },
-                      body: encode({ "form-name": "hck2contact", ...state }),
-                    })
-                      .then(() => {
-                        console.log("submitted")
-                        updSubd(true)
+              <div className={styles.form_container}>
+                {subd ? (
+                  <span>Thx bro!</span>
+                ) : (
+                  <form
+                    name="hck2contact"
+                    className={styles.newsletterForm}
+                    onSubmit={e => {
+                      fetch("/", {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/x-www-form-urlencoded",
+                        },
+                        body: encode({ "form-name": "hck2contact", ...state }),
                       })
-                      .catch(err => alert(err))
-                    e.preventDefault()
-                  }}
-                  data-netlify="true"
-                  data-netlify-honeypot="bot-field"
-                >
-                  <div className={styles.form_group}>
-                    <label htmlFor="first_name">
-                      First Name<sup>*</sup>
-                    </label>
-                    <input type="text" name="first_name" />
-                  </div>
-
-                  <div className={styles.form_group}>
-                    <label htmlFor="last_name">
-                      Last Name<sup>*</sup>
-                    </label>
-                    <input type="text" name="last_name" />
-                  </div>
-
-                  <div className={styles.form_group}>
-                    <label htmlFor="type">
-                      Inquiry Type<sup>*</sup>
-                    </label>
-                    <div className={styles.select_wrapper}>
-                      <select name="type">
-                        <option value="general">General Information</option>
-                        <option value="rfp">Request for Proposal</option>
-                        <option value="career">Career Opportunities</option>
-                      </select>
+                        .then(() => {
+                          console.log("submitted")
+                          updSubd(true)
+                        })
+                        .catch(err => alert(err))
+                      e.preventDefault()
+                    }}
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                  >
+                    <div className={styles.form_group}>
+                      <label htmlFor="first_name">
+                        First Name<sup>*</sup>
+                      </label>
+                      <input type="text" name="first_name" />
                     </div>
-                  </div>
 
-                  <div className={styles.form_group}>
-                    <label htmlFor="organization">
-                      Organization<sup>*</sup>
-                    </label>
-                    <input type="text" name="organization" />
-                  </div>
+                    <div className={styles.form_group}>
+                      <label htmlFor="last_name">
+                        Last Name<sup>*</sup>
+                      </label>
+                      <input type="text" name="last_name" />
+                    </div>
 
-                  <div className={styles.form_group}>
-                    <label htmlFor="email">
-                      Email<sup>*</sup>
-                    </label>
-                    <input type="email" name="email" />
-                  </div>
+                    <div className={styles.form_group}>
+                      <label htmlFor="type">
+                        Inquiry Type<sup>*</sup>
+                      </label>
+                      <div className={styles.select_wrapper}>
+                        <select name="type">
+                          <option value="general">General Information</option>
+                          <option value="rfp">Request for Proposal</option>
+                          <option value="career">Career Opportunities</option>
+                        </select>
+                      </div>
+                    </div>
 
-                  <div className={styles.form_group}>
-                    <label htmlFor="phone">Phone</label>
-                    <input type="phone" name="phone" />
-                  </div>
+                    <div className={styles.form_group}>
+                      <label htmlFor="organization">
+                        Organization<sup>*</sup>
+                      </label>
+                      <input type="text" name="organization" />
+                    </div>
 
-                  <div className={styles.form_group}>
-                    <label htmlFor="comments">Question / Comments</label>
-                    <textarea name="comments"></textarea>
-                    <p className={styles.small}>*Required</p>
-                  </div>
+                    <div className={styles.form_group}>
+                      <label htmlFor="email">
+                        Email<sup>*</sup>
+                      </label>
+                      <input type="email" name="email" />
+                    </div>
 
-                  <input type="hidden" name="form-name" value="hck2contact" />
-                  <div className={styles.form_group}>
-                    <input type="submit" value="SUBMIT" />
-                  </div>
-                </form>
-              )}
+                    <div className={styles.form_group}>
+                      <label htmlFor="phone">Phone</label>
+                      <input type="phone" name="phone" />
+                    </div>
+
+                    <div className={styles.form_group}>
+                      <label htmlFor="comments">Question / Comments</label>
+                      <textarea name="comments"></textarea>
+                      <p className={styles.small}>*Required</p>
+                    </div>
+
+                    <input type="hidden" name="form-name" value="hck2contact" />
+                    <div className={styles.form_group}>
+                      <input type="submit" value="SUBMIT" />
+                    </div>
+                  </form>
+                )}
+              </div>
             </div>
             <div
               className={[styles.col_12, styles.col_md_6, styles.col].join(" ")}
@@ -165,7 +167,7 @@ const mapQuery = graphql`
     staticMap {
       childFile {
         childImageSharp {
-          fixed(width: 300) {
+          fixed(width: 400) {
             base64
             width
             height
