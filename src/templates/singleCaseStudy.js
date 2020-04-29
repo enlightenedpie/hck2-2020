de20adb29a4f289ba639c8f6aff4aeaf87565111
David Paul Crouch <david.crouch@hck2.com>
2020-04-28 20:55:47 -0500

    pushing new changes for case study page builds

--- src/templates/singleCaseStudy.js
+++ src/templates/singleCaseStudy.js
@@ -1,22 +1,13 @@
 import React from "react"
-import HtmlToReact from "html-to-react"
-//import { graphql } from "gatsby"
+// import HtmlToReact from "html-to-react"
+// import { graphql } from "gatsby"
 
-import Layout from "./layout"
+// import Layout from "./layout"
 
-import styles from "./single.module.sass"
+// import styles from "./single.module.sass"
 
-const HTR = new HtmlToReact.Parser()
+// const HTR = new HtmlToReact.Parser()
 
-export default ({ pathContext: post }) => {
-  let { featuredImage: image, content, seo } = post
-
-  return (
-    <Layout bodyClass="single single-post" seo={seo}>
-      <section className={styles.singleStage}>
-        {/* Hero image goes here */}
-      </section>
-      <section className={styles.singleContent}>{HTR.parse(content)}</section>
-    </Layout>
-  )
+export default props => {
+  return <div></div>
 }
