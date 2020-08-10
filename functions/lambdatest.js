5c7bea4298c8202519999f5fa7eb2c2c600de9e1
David Paul <enlightenedpie@gmail.com>
2020-08-10 16:49:42 -0500

    testing new lambda function

--- functions/lambdatest.js
+++ functions/lambdatest.js
@@ -1,10 +1,14 @@
 const path = require("path")
 const fs = require("fs")
 
-var years = fs.readFile(path.resolve("./store/years.json"), (err, data) => {
-  if (err) throw err
-  return data
-})
+var years = fs.readFile(
+  path.resolve(__dirname) + "/store/years.json",
+  (err, data) => {
+    if (err) throw err
+    console.log(data)
+    return data
+  }
+)
 
 exports.handler = (event, context, callback) => {
   callback(null, {
